10c5b1118effd0e1b113cb5053356d657a88cc35
BobHy <bob.hyman@gmail.com>
2022-11-27 22:00:53 -0800

    initial commit

---
+++ go.mod
@@ -0,0 +1,14 @@
+module example/watcher 
+
+go 1.19
+
+require (
+	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/rs/zerolog v1.28.0 // indirect
+	go.uber.org/atomic v1.10.0 // indirect
+	go.uber.org/multierr v1.8.0 // indirect
+	go.uber.org/zap v1.23.0 // indirect
+	golang.org/x/sys v0.0.0-20220908164124-27713097b956 // indirect
+)
---
+++ watcher.go
@@ -0,0 +1,86 @@
+/*
+A package comment
+*/
+package main
+
+import (
+	"flag"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/fsnotify/fsnotify"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log" // single, global logger
+)
+
+// MyWatcher is merely a convenient alias
+// N.B -- capitalized, so exported.
+type MyWatcher fsnotify.Watcher // a semi-convenient alias
+
+// Main Command line monitors a specified directory and dumps
+// fsnotify.Write events.
+//
+// it takes No arguments
+func main() {
+
+	verbose := flag.Bool("v", false, "enable for more detail")
+	flag.Parse()
+
+	if flag.NArg() != 1 {
+		flag.Usage()
+		os.Exit(1)
+		// and exit
+	}
+
+	log.Logger = log.Output(zerolog.ConsoleWriter{
+		Out: os.Stderr, TimeFormat: time.RFC3339})
+
+	if *verbose {
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	} else {
+		zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	}
+
+	watchPath, err := filepath.Abs(flag.Arg(0))
+	if err != nil {
+		log.Fatal(). // will exit
+				Err(err).
+				Str("path", flag.Arg(0)).
+				Msg("FilePath error")
+		//log.Fatalf("Error on %s: %s", flag.Arg(0), err)
+	}
+
+	log.Debug().
+		Str("path", watchPath).
+		Msg("Watching writes")
+
+	watcher, err := fsnotify.NewWatcher()
+
+	if err != nil {
+		log.Fatal().Err(err).Msg("NewWatcher err")
+	}
+
+	defer watcher.Close()
+
+	done := make(chan bool)
+
+	go func() {
+		for {
+			select {
+			case event := <-watcher.Events:
+				if event.Op&fsnotify.Write == fsnotify.Write {
+					log.Info().Str("path", event.Name).Msg("modified file")
+				}
+			case err := <-watcher.Errors:
+				log.Warn().Err(err).Msg("error from watcher")
+			}
+		}
+	}()
+
+	err = watcher.Add(watchPath)
+	if err != nil {
+		log.Fatal().Err(err).Msg("watcher.add")
+	}
+	<-done
+}
